a41f726b500b4ce8f9be1fd8feed71aa8240b049
Rishabh Naharwal <rishabh.naharwal@gmail.com>
2024-12-03 18:12:50 +0530

    Update Number Guessing GAME Play it FaaST!!.c

--- Number Guessing GAME Play it FaaST!!.c
+++ Number Guessing GAME Play it FaaST!!.c
@@ -6,7 +6,7 @@ int number[3], num[3], sum[3], min, r, i;
 for (i=0; i<3; i++) {
     sum[i]=0;
 }
-for (i=0; i<2; i++) {
+for (i=0; i<3; i++) {
 printf("PLAYER %d CHANCE\n", i+1);
 srand(time(0));
 number[i]=rand()%100+101; //generates a random number between 101 and 200
@@ -26,7 +26,7 @@ while (num[i]!=number[i]) {
 if (num[i]==number[i]) printf("You guessed it right\nNEXT PLAYER\n\n");
 }
 min=sum[0];
-for (i=0; i<2; i++) {
+for (i=0; i<3; i++) {
     if (sum[i]<=min) {
         min=sum[i];
         r=i;
